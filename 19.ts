874de57a455edf2451bcd7e4f7d36455c625342c
Nikhil Yekhe <yendenikhil@gmail.com>
2020-12-19 21:51:17 +0100

    day 19 done

--- 19.ts
+++ 19.ts
@@ -1,47 +1,70 @@
-const raw = await Deno.readTextFile('19.in')
-const p = console.log
+const raw = await Deno.readTextFile("19.in");
+const p = console.log;
 
-const match = (ruleId: string, input: string, rules: Map<string, string[]>) => {
-    const rule = rules.get(ruleId) ?? []
-    // p({ ruleId,rule, input })
-    if (rule.length === 1 && rule[0].search(/[a-z]/) > -1) {
-        if (input[0] === rule[0]) return input.substring(1)
-        else return input
-    } else {
-        let smallest = input
-        for (const r of rule) {
-            const rlist = r.split(' ')
-            let test = input
-            for (const rr of rlist) {
-                test = match(rr, test, rules)
-            }
-            if (test.length < smallest.length) smallest = test
-        }
-    return smallest
+const compose = (id: string, rules: Map<string, string>): string => {
+  let rule = rules.get(id) ?? "";
+  let counter = 0;
+  while (rule.search(/\d/) > -1) {
+    counter++;
+    // if (counter > 10) break
+    rule = rule.replace(/\d+/, (m) => {
+      return compose(m, rules);
+    });
+  }
+  // p({id, rule})
+  return rule;
+};
+
+const expand = (rule: string, iter: number): string => {
+  let str = rule;
+  const rep = (s: string, num: number) => {
+    let ans = s;
+    for (let i = 0; i < num; i++) {
+      ans += " " + s;
     }
-}
+    return ans;
+  };
+  for (let i = 1; i <= iter; i++) {
+    str += " | ";
+    str += rule.split(" ").map((e) => rep(e, i)).join(" ");
+  }
+  return str;
+};
 
 const part = (raw: string, isPart2 = true) => {
-    const rules: Map<string, string[]> = new Map()
-    let isRule = true
-    let ans = 0
-    for (const line of raw.split('\n')) {
-        if (line.length === 0) {
-            isRule = false
-            continue
-        }
-        if (isRule) {
-            const [num, rule] = line.split(": ")
-            const ruleset = rule.replaceAll('"', '').split(" | ")
-            if (isPart2 && num === '8') ruleset.push('42 8')
-            if (isPart2 && num === '11') ruleset.push('42 11 31')
-            rules.set(num, ruleset)
-        } else {
-            ans += match('0', line, rules).length === 0 ? 1 : 0
-        }
+  const rules: Map<string, string> = new Map();
+  let isRule = true;
+  const inputs = [];
+  // let ans = 0
+  for (const line of raw.split("\n")) {
+    if (line.length === 0) {
+      isRule = false;
+      continue;
     }
-    p({ ans })
+    if (isRule) {
+      const [num, rule] = line.split(": ");
+      if (rule.includes('"')) {
+        rules.set(num, rule[1]);
+      } else {
+        if (isPart2 && (num === "8" || num === "11")) {
+          const ex = expand(rule, 4);
+          rules.set(num, "(" + ex + ")");
+        } else {
+          rules.set(num, "(" + rule + ")");
+        }
+      }
+    } else {
+      inputs.push(line);
+    }
+  }
+  const expression = compose("0", rules).replaceAll(" ", "").replace(/^/, "^")
+    .replace(/$/, "$");
+  const ans = inputs.filter((e) => e.search(expression) > -1).length;
+  p({ ans });
+};
 
-}
-
-part(raw, false)
+console.time("p");
+part(raw, false);
+console.timeLog("p");
+part(raw, true);
+console.timeEnd("p");
